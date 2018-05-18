84411615c6bc53a922ec89143e9e825da496b74e
gurisaka <guri95saka22@gmail.com>
2018-05-18 15:30:27 +0900

    再帰回数制限実装

--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import re
 import sys
 from pprint import pprint
 
-def search_files(dir_path,file_name_pattern):
+def search_files(dir_path,file_name_pattern,now_dir_depth,max_dir_depth):
+	#Depth Check
+	if now_dir_depth >= max_dir_depth:
+		return []
+
 	child_dirs = []
 	files = []
 
 	root_path = dir_path
 
-	target_file_path = (root_path.replace('\\','\\\\') + file_name_pattern)
+	target_file_path = (root_path + file_name_pattern)
 	target_file_path_pattern = re.compile(r'%s' % target_file_path)
 
+	#Search Dir and Files
 	for path in os.listdir(root_path):
-		absolute_path = root_path + '\\' +path
+		absolute_path = root_path + '/' +path
 		if os.path.isdir(absolute_path):
 			child_dirs.append(absolute_path)
 		else:
@@ -21,7 +26,7 @@ def search_files(dir_path,file_name_pattern):
 				files.append(absolute_path)
 
 	for child_dir in child_dirs:
-		child_dir_files = search_files(child_dir.replace('\\\\','\\'),file_name_pattern)
+		child_dir_files = search_files(child_dir,file_name_pattern,now_dir_depth + 1,max_dir_depth)
 		for file in child_dir_files:
 			files.append(file)
 
@@ -33,12 +38,21 @@ def replace_new_line_code(file_name):
 def main():
 	try:
 		file_name_pattern = sys.argv[1]
+		try:
+			max_dir_depth = int(sys.argv[2])
+			if max_dir_depth >= 7:
+				print("over 7 depth is invalid !!")
+				max_dir_depth = 6
+		except:
+			max_dir_depth = 4
 	except:
 		print('Input File Name Pattern !!')
 		return False
 	
 	root_path = os.getcwd()
-	files = search_files(root_path,file_name_pattern)
+
+	files = search_files(root_path,file_name_pattern,0,max_dir_depth)
+
 	pprint(files)
 	for file in files:
 		replace_new_line_code(file)
